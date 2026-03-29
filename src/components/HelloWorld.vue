14fa479b5a0189017a71569ac998b91050c5e34b
Cristian Guzmán <cristian.guzman.contacto@gmail.com>
2026-03-29 14:44:32 -0600

    🐛 tailwind configured

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -11,8 +11,12 @@ onMounted(async () => {
 </script>
 
 <template>
-
-	<div>
-		{{ VIX }}
+	<div class="bg-white shadow-md rounded-2xl p-6 w-80">
+		<h2 class="text-xl font-semibold text-gray-800 mb-2">
+			VIX
+		</h2>
+		<p class="text-gray-500 text-sm mb-4">
+			{{ VIX }}
+		</p>
 	</div>
 </template>
